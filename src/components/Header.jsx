0e927b8288d69b714434c6b0163a0ea3b1d256c7
andykr1k <69021102+andykr1k@users.noreply.github.com>
2022-07-14 12:39:06 -0700

    Nice Simple Design

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,14 +1,16 @@
 import logo from '../assets/rbanimoji.png'
+import { motion } from "framer-motion"
+
 const Header = () => {
 
   return (
-    <div className='flex text-white bg-slate-800'>
-        <img src={logo} alt="Logo" />
+    <div className='flex text-white bg-slate-800 bg-opacity-10'>
+        <motion.img whileHover={{ scale: 1.2 }} src={logo} alt="Logo" />
         <div className='flex'>
-            <a className='m-auto mr-5 hover:underline'>Home</a>
-            <a className='m-auto mr-5'>About Me</a>
-            <a className='m-auto mr-5'>Projects</a>
-            <a className='m-auto mr-5'>Contact</a>
+            <motion.a whileHover={{ scale: 1.2 }} className='m-auto mr-5'>Home</motion.a>
+            <motion.a whileHover={{ scale: 1.2 }} className='m-auto mr-5'>About Me</motion.a>
+            <motion.a whileHover={{ scale: 1.2 }} className='m-auto mr-5'>Projects</motion.a>
+            <motion.a whileHover={{ scale: 1.2 }} className='m-auto mr-5'>Contact Me</motion.a>
         </div>
     </div>
   )
